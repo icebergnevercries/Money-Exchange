3fecbe0793c37b3dcd4bbfb3287a457cc295078e
icebergnevercries <denislosik@mail.ru>
2018-09-25 01:42:09 +0300

    implementation function, which exchanges currency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const namesCoin = ['H', 'Q', 'D', 'N', 'P'];
+    const valOfCoins = [50, 25, 10, 5, 1];
+    let arrayCoins = {};
+
+    if(currency <= 0)
+    {
+        return [];
+    }
+
+    if(currency >= 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    for(let i = 0; i < 5; i++)
+    {
+        let massCurrEx = currencyExchange(valOfCoins[i], currency);
+        if(massCurrEx[0]!=0){
+            arrayCoins[namesCoin[i]] = massCurrEx[0];
+            currency = massCurrEx[1];
+        }                 
+    }
+
+    function currencyExchange (value, currency){
+        let count = 0;
+        while(currency / value >= 1){
+            currency -= value;
+            count++;
+        }
+        return [count, currency];
+    }
+
+    return arrayCoins;
 }
